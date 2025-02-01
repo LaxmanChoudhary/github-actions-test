8e54f44383123c74afe5780dba23fa54e7138a1c
LaxmanChoudhary <laxmanchoudhary6811@gmail.com>
2025-02-01 19:01:31 +0530

    Revert "test"
    This reverts commit 5a2fe046cf39902161a63050de2ed1f74d308a7f.

--- scripts/update_changelog.py
+++ scripts/update_changelog.py
@@ -1,15 +1,7 @@
-import datetime as dt
 import os
-import re
-import subprocess
-from collections.abc import Iterable
 from pathlib import Path
 
-import git
-import github.PullRequest
-import github.Repository
 from github import Github
-from jinja2 import Template
 
 CURRENT_FILE = Path(__file__)
 ROOT = CURRENT_FILE.parents[1]
@@ -21,26 +13,9 @@ GIT_BRANCH = os.getenv("GITHUB_REF_NAME")
 def main() -> None:
     try:
         print("Some data", CURRENT_FILE, ROOT, "token yes" if GITHUB_TOKEN else "token no", GITHUB_REPO, GIT_BRANCH)
-        merged_date = dt.date.today() - dt.timedelta(days=1)
         repo = Github(login_or_token=GITHUB_TOKEN).get_repo(GITHUB_REPO)
-        merged_pulls = list(iter_pulls(repo, merged_date))
-        print("end")
     except Exception as ex:
         print("Some exception occurred when reading Github", ex)
-
-def iter_pulls(
-    repo: github.Repository.Repository,
-    merged_date: dt.date,
-) -> Iterable[github.PullRequest.PullRequest]:
-    """Fetch merged pull requests at the date we're interested in."""
-    recent_pulls = repo.get_pulls(
-        state="closed",
-        sort="updated",
-        direction="desc",
-    ).get_page(0)
-    for pull in recent_pulls:
-        if pull.merged and pull.merged_at.date() == merged_date:
-            yield pull
 
 
 if __name__ == "__main__":
